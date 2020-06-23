ffb21bf6944bd5dd18449cc7e69d114dd2ed9d13
joshroche1 <joshua.a.roche@gmail.com>
2020-06-23 09:53:42 +0200

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# run as root
+cd /opt
+apt-get update
+apt-get upgrade -y
+apt-get install default-jdk maven unzip
+wget http://apache.mirrors.pair.com/tomee/tomee-8.0.2/apache-tomee-8.0.2-webprofile.zip
+unzip apache-tomee-8.0.2-webprofile.zip
+mv apache-tomee-webprofile-8.0.2/ tomee/
+iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
+iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 8443
+iptables-save
+useradd tomee
+username=tomee
+PASSWORD=ApacheTomEE
+echo "$PASSWORD" | passwd --stdin "tomee"
+cp tomee.service /etc/systemd/system/
+systemctl daemon-reload 
+systemctl tomee start 
+cd /opt/tomee/bin 
+keytool -genkey -alias tomee -keyalg RSA $PASSWORD
+cp server.xml /opt/tomee/conf/server.xml
+cp tomcat-users.xml /opt/tomee/conf/tomcat-users.xml
+cp context.xml /opt/tomee/webapp/manager/META-INF/context.xml
+cp context.xml /opt/tomee/webapp/host-manager/META-INF/context.xml
+systemctl restart tomee 
