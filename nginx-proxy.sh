7097f56ab5d8ffde125d21ea36f80ddefbf00ffe
joshroche1 <joshua.a.roche@gmail.com>
2021-11-03 19:56:51 +0100

    Update nginx-proxy.sh

--- nginx-proxy.sh
+++ nginx-proxy.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
 sudo apt install nginx
-sudo snap install certbot --classic
+openssl \
+    req \
+    -nodes \
+    -newkey rsa:4096 \
+    -keyout server.key \
+    -out server.csr \
+    -subj "/C=DE/ST=NRW/L=Berlin/O=My Inc/OU=DevOps/CN=Server/emailAddress=admin@localhost"
+openssl x509 -req -days 3650 -in server.csr -signkey server.key -out server.crt
+mv server.crt /etc/ssl/certs
+mv server.key /etc/ssl/private
+rm server.csr
