a93c535375e07b873be0587156d653ba5ac2508b
joshroche1 <joshua.a.roche@gmail.com>
2022-02-25 19:03:57 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install default-jdk maven
-wget https://downloads.apache.org/tomee/tomee-9.0.0-M7/apache-tomee-9.0.0-M7-webprofile.tar.gz
-tar xvzf apache-tomee-9.0.0-M7-webprofile.tar.gz
-sudo mv apache-tomee-webprofile-9.0.0-M7/ /opt/tomee/
-#
+apt update; apt upgrade -y;  apt install default-jdk unzip maven -y
+cd /opt
+wget https://downloads.apache.org/tomee/tomee-8.0.10/apache-tomee-8.0.10-webprofile.zip
+unzip xvzf apache-*
+ln -s apache-tomee-web* tomee/
 #iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
 #iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 8443
 #iptables-save
-#
-sudo mv /opt/tomee/conf/tomcat-users.xml /opt/tomee/conf/tomcatusers.xml.OLD
-sudo mv tomcat-users.xml /opt/tomee/conf/
-sudo mv /opt/tomee/webapps/manager/META-INF/context.xml /opt/tomee/webapps/manager/META-INF/context.xml.OLD
-sudo cp context.xml /opt/tomee/webapps/manager/META-INF/
-sudo mv /opt/tomee/webapps/host-manager/META-INF/context.xml /opt/tomee/webapps/host-manager/META-INF/context.xml.OLD
-sudo mv context.xml /opt/tomee/webapps/host-manager/META-INF/
-sudo mv tomee.service /etc/systemd/system/
-sudo systemctl daemon-reload
-sudo systemctl enable tomee.service
-sudo systemctl start tomee.service
-sudo systemctl status tomee.service
+chmod 0744 /opt/tomee/bin/*.sh
+cp tomee.service /etc/systemd/system/
+cp tomcat-users.xml /opt/tomee/conf/tomcat-users.xml
+cp context.xml /opt/tomee/webapps/manager/META-INF/context.xml
+cp context.xml /opt/tomee/webapps/host-manager/META-INF/context.xml
+#cd /opt/tomee/bin
+#keytool -genkey -alias tomee -keyalg RSA -storepass ApacheTomEE
+systemctl daemon-reload
+systemctl enable tomee.service
+systemctl start tomee.service
