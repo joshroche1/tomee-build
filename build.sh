2c49f7a2b15c6b7ffa8b877bebf62c3a0266a287
joshroche1 <joshua.a.roche@gmail.com>
2022-06-20 19:51:22 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,12 +2,15 @@
 
 apt update; apt upgrade -y;  apt install default-jdk unzip maven -y
 cd /opt
-wget https://downloads.apache.org/tomee/tomee-8.0.10/apache-tomee-8.0.10-webprofile.zip
+wget https://downloads.apache.org/tomee/tomee-8.0.12/apache-tomee-8.0.12-webprofile.zip
 unzip xvzf apache-*
 ln -s apache-tomee-web* tomee/
 #iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
 #iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 8443
 #iptables-save
+#useradd -p $(openssl passwd -crypt ApacheTomEE) tomee
+#mkdir /home/tomee
+#chown -R tomee: /home/tomee
 chmod 0744 /opt/tomee/bin/*.sh
 cp tomee.service /etc/systemd/system/
 cp tomcat-users.xml /opt/tomee/conf/tomcat-users.xml
